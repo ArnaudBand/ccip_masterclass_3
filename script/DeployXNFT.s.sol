f68bbe3a1f7a0396d08bd2e55cdc55d40406ddc8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-08-13 08:38:06 +0000

    feat: Update DeployXNFT script to use HelpConfig for network configuration

--- script/DeployXNFT.s.sol
+++ script/DeployXNFT.s.sol
@@ -1,23 +1,30 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: LicenseRef-LICENSE
 pragma solidity ^0.8.19;
 
-import "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {XNFT} from "../src/XNFT.sol";
+import {HelpConfig} from "./HelpConfig.s.sol";
+// import {HelpConfig.NetworkConfig} from "./HelpConfig.s.sol";
 
 contract DeployXNFT is Script {
-    function run() public {
+    function run() external returns (XNFT, HelpConfig) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        address ccipRouterAddressEthereumSepolia = 0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59;
-        address linkTokenAddressEthereumSepolia = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-        uint64 chainSelectorEthereumSepolia = 16015286601757825753;
+        HelpConfig helpConfig = new HelpConfig();
+        
+        // Directly access the struct components
+        (address ccipRouterAddress, address linkTokenAddress, uint64 chainSelector) = helpConfig.activeConfig();
 
-        XNFT xNft =
-            new XNFT(ccipRouterAddressEthereumSepolia, linkTokenAddressEthereumSepolia, chainSelectorEthereumSepolia);
+        XNFT xNft = new XNFT(
+            ccipRouterAddress,
+            linkTokenAddress,
+            chainSelector
+        );
 
         console.log("XNFT deployed to ", address(xNft));
 
         vm.stopBroadcast();
+        return (xNft, helpConfig);
     }
 }
